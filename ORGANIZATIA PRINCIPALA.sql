17a3b72a75154de2f5b7868a9f8dd737b4dc6e1b
maximilianstan <maximilian.s@outlook.com>
2023-08-23 20:31:25 +0300

    Update ORGANIZATIA PRINCIPALA.sql
    table additions

--- ORGANIZATIA PRINCIPALA.sql
+++ ORGANIZATIA PRINCIPALA.sql
@@ -1,2 +1,73 @@
 CREATE DATABASE IF NOT EXISTS organizatia_principala;
 USE organizatia_principala;
+CREATE TABLE IF NOT EXISTS magazine (
+id TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+denumire VARCHAR(50),
+telefon CHAR(10) NOT NULL UNIQUE,
+faliment ENUM ("Da", "Nu") NOT NULL,
+locatie VARCHAR(100),
+cifra_afaceri CHAR(10) NOT NULL
+);
+DESCRIBE magazine;
+CREATE TABLE IF NOT EXISTS angajati (
+id TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nume VARCHAR(100) NOT NULL,
+prenume VARCHAR(100) NOT NULL,
+telefon CHAR(10),
+CNP CHAR(13) NOT NULL UNIQUE,
+stare_civila ENUM ('Necasatorit', 'Casatorit', 'Divortat'),
+e_mail VARCHAR(100),
+departament VARCHAR(80),
+salariu MEDIUMINT,
+data_angajarii DATE,
+id_magazin TINYINT,
+FOREIGN KEY (id_magazin) REFERENCES magazine(id)
+);
+DESCRIBE angajati;
+CREATE TABLE IF NOT EXISTS furnizori (
+id TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+denumire VARCHAR(50) NOT NULL,
+locatie VARCHAR(100),
+telefon CHAR(10)
+);
+DESCRIBE furnizori;
+
+CREATE TABLE IF NOT EXISTS produse (
+id MEDIUMINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+brand VARCHAR(100) NOT NULL,
+denumire VARCHAR(200) NOT NULL,
+pret_lei MEDIUMINT,
+id_furnizor TINYINT,
+id_magazin TINYINT,
+stoc_furnizor ENUM ("Da", "Nu"),
+FOREIGN KEY (id_furnizor) REFERENCES furnizori(id),
+FOREIGN KEY (id_magazin) REFERENCES magazine(id)
+);
+
+DESCRIBE produse;
+
+CREATE TABLE IF NOT EXISTS clienti (
+id TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nume VARCHAR(100) NOT NULL,
+prenume VARCHAR(100) NOT NULL,
+telefon CHAR(10),
+locatie VARCHAR(100),
+e_mail VARCHAR(100)
+);
+DESCRIBE clienti;
+
+CREATE TABLE IF NOT EXISTS comenzi (
+id TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+total_plata_lei MEDIUMINT,
+plata_cash BOOL,
+id_magazin TINYINT,
+id_produs MEDIUMINT,
+id_client TINYINT,
+FOREIGN KEY (id_magazin) REFERENCES magazine(id),
+FOREIGN KEY (id_produs) REFERENCES produse(id),
+FOREIGN KEY (id_client) REFERENCES clienti(id)
+);
+
+DESCRIBE comenzi;
+
+
